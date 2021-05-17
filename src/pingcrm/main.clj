5cdf4b400e290fbd3bbdbbbdd49b0394472f654d
Michael SALIHI <admin@prestance-informatique.fr>
2021-05-17 22:51:31 +0200

    Add middleware for settings shared props

--- src/pingcrm/main.clj
+++ src/pingcrm/main.clj
@@ -7,7 +7,10 @@
             [reitit.ring :as ring]
             [reitit.ring.middleware.parameters :as params]
             [ring.adapter.jetty :as server]
-            [ring.middleware.keyword-params :refer [wrap-keyword-params]]))
+            [ring.middleware.flash :refer [wrap-flash]]
+            [ring.middleware.keyword-params :refer [wrap-keyword-params]]
+            [ring.middleware.session :refer [wrap-session]]
+            [ring.middleware.session.memory :refer [memory-store]]))
 
 (def asset-version "1")
 
@@ -26,12 +29,15 @@
     [:div {:id "app"
            :data-page data-page}]]))
 
-(def share
-  (let [user (db/get-user-by-id 1)]
-    {:errors {}
-     :auth {:user user}
-     :flash {:success nil
-             :error nil}}))
+(defn wrap-inertia-share [handler]
+  (fn [request]
+    (let [user (db/get-user-by-id 1)
+          success (-> request :flash :success)
+          props {:errors {}
+                 :auth {:user user}
+                 :flash {:success success
+                         :error nil}}]
+      (handler (assoc request :inertia-share props)))))
 
 (defn index [_]
   (inertia/render "Dashboard/Index"))
@@ -39,31 +45,34 @@
 (defn reports [_]
   (inertia/render "Reports/Index"))
 
-(defn error []
-  "trigger error")
-
 (def app
   (ring/ring-handler
    (ring/router
     [["/" {:get {:handler #'index}}]
      ["/users"
-      ["" {:get {:handler #'users/get-users
-                 :parameters {:query {:search int?}}}}]
-      ["/:user-id" {:post {:handler #'users/update-user!
-                           :parameters {:body {:first_name string?
-                                               :last_name string?
-                                               :email string?
-                                               :owner boolean?}}}}]
-      ["/:user-id/edit" {:get {:handler #'users/edit-user!
-                               :parameters {:query {:user-id int?}}}}]]
-     ["/reports" {:get {:handler #'reports}}]])
+      [""
+       {:get {:handler    #'users/get-users
+              :parameters {:query {:search int?}}}}]
+      ["/:user-id"
+       {:post {:handler    #'users/update-user!
+               :parameters {:body {:first_name string?
+                                   :last_name  string?
+                                   :email      string?
+                                   :owner      boolean?}}}}]
+      ["/:user-id/edit"
+       {:get {:handler    #'users/edit-user!
+              :parameters {:query {:user-id int?}}}}]]
+     ["/reports" {:get {:handler #'reports}}]]
+    {:data {:middleware [params/parameters-middleware
+                         wrap-keyword-params
+                         [wrap-session {:store (memory-store)}]
+                         wrap-flash
+                         wrap-inertia-share
+                         [inertia/wrap-inertia template asset-version]]}})
    (ring/routes
     (ring/create-resource-handler {:path "/"})
     (ring/create-default-handler
-     {:not-found (constantly {:status 404, :body error/not-found})}))
-   {:middleware [params/parameters-middleware
-                 wrap-keyword-params
-                 [inertia/wrap-inertia template asset-version share]]}))
+     {:not-found (constantly {:status 404, :body error/not-found})}))))
 
 (defn -main []
   (server/run-jetty #'app {:port 3000
