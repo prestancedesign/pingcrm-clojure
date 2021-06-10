703bbb60d87303ea0b67c0b386c7e5dd347f88e0
Michael SALIHI <admin@prestance-informatique.fr>
2021-06-10 17:20:41 +0200

    Add PostCSS, Tailwind config and package.json build scripts setup

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+// postcss.config.js
+module.exports = {
+    plugins: [
+        require('postcss-import'),
+        require('tailwindcss'),
+        require('autoprefixer'),
+        // process.env.NODE_ENV === 'production' ? require('cssnano') : '',
+    ]
+}
---
+++ tailwind.config.js
@@ -0,0 +1,48 @@
+const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+    purge: ['./resources/public/js/app.js',
+            'src/pingcrm/templates/app.clj'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      red: colors.red,
+      orange: colors.orange,
+      yellow: colors.yellow,
+      green: colors.green,
+      gray: colors.blueGray,
+      indigo: {
+        100: '#e6e8ff',
+        300: '#b2b7ff',
+        400: '#7886d7',
+        500: '#6574cd',
+        600: '#5661b3',
+        800: '#2f365f',
+        900: '#191e38',
+      },
+    },
+    extend: {
+      borderColor: theme => ({
+        DEFAULT: theme('colors.gray.200', 'currentColor'),
+      }),
+      fontFamily: {
+        sans: ['Cerebri Sans', ...defaultTheme.fontFamily.sans],
+      },
+      boxShadow: theme => ({
+        outline: '0 0 0 2px ' + theme('colors.indigo.500'),
+      }),
+      fill: theme => theme('colors'),
+    },
+  },
+  variants: {
+    extend: {
+      fill: ['focus', 'group-hover'],
+    },
+  },
+  plugins: [],
+}
