5df0461e974fa3d24ea974bc1639dbf2caa8df95
Michael SALIHI <admin@prestance-informatique.fr>
2021-06-11 02:51:18 +0200

    Postcss-nesting setup

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@
 module.exports = {
     plugins: [
         require('postcss-import'),
+        require('postcss-nesting'),
         require('tailwindcss'),
         require('autoprefixer'),
         // process.env.NODE_ENV === 'production' ? require('cssnano') : '',
