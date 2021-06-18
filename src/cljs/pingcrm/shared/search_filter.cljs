0eee9386cffea3ccfab9aece152a8c0ba452eccf
Michael SALIHI <admin@prestance-informatique.fr>
2021-06-18 10:22:52 +0200

    Revert "Refactor search filter function (form-3 -> form-2 component)"
    This reverts commit 4ea37cb677e7c93fea4b79bce62edcde2b946f4d.

--- src/cljs/pingcrm/shared/search_filter.cljs
+++ src/cljs/pingcrm/shared/search_filter.cljs
@@ -3,14 +3,14 @@
             ["@inertiajs/inertia-react" :refer [usePage]]
             [applied-science.js-interop :as j]
             [goog.functions :refer [throttle]]
-            [pingcrm.shared.form-input :refer [select-input]]
+            [pingcrm.shared.form-input :refer [text-input select-input]]
             [reagent.core :as r]))
 
 (defn filters-query [values]
   (let [query (into {} (filter (comp not-empty val) values))
         options #js {:replace true
                      :preserveState true}]
-    (throttle (.get Inertia (.current (js/route)) (clj->js query) options) 150)))
+    (.get Inertia (.current (js/route)) (clj->js query) options)))
 
 (defn search-filter []
   (let [{:keys [filters]} (j/lookup (.-props (usePage)))
@@ -20,55 +20,60 @@
         opened? (r/atom false)
         on-change #(let [key (keyword (.. % -target -name))
                          value (.. % -target -value)]
-                     (swap! values assoc key value)
-                     (filters-query @values)
-                     (when opened? (reset! opened? false)))
+                    (swap! values assoc key value)
+                    (when opened? (reset! opened? false)))
         reset #(reset! values {:search ""
                                :role ""
                                :trashed ""})]
-    (fn []
-      [:div {:class "flex items-center w-full max-w-md mr-4"}
-       [:div {:class "relative flex w-full bg-white rounded shadow"}
-        [:div {:style {:top "100%"}
-               :class (str "absolute" (when-not @opened? " hidden"))}
-         [:div {:class "fixed inset-0 z-20 bg-black opacity-25"
-                :on-click #(reset! opened? false)}]
-         [:div {:class "relative z-30 w-64 px-4 py-6 mt-2 bg-white rounded shadow-lg"}
-          (when (.hasOwnProperty filters "role")
+    (r/create-class
+     {:display-name "search-filter"
+      :component-did-update
+      (fn []
+        (throttle (filters-query @values) 150))
+      :reagent-render
+      (fn []
+        [:div {:class "flex items-center w-full max-w-md mr-4"}
+         [:div {:class "relative flex w-full bg-white rounded shadow"}
+          [:div {:style {:top "100%"}
+                 :class (str "absolute" (when-not @opened? " hidden"))}
+           [:div {:class "fixed inset-0 z-20 bg-black opacity-25"
+                  :on-click #(reset! opened? false)}]
+           [:div {:class "relative z-30 w-64 px-4 py-6 mt-2 bg-white rounded shadow-lg"}
+            (when (.hasOwnProperty filters "role")
+              [select-input
+               {:class "mb-4"
+                :label "Role"
+                :name "role"
+                :value (:role @values)
+                :on-change on-change}
+               [:option {:value ""}]
+               [:option {:value "user"} "User"]
+               [:option {:value "owner"} "Owner"]])
             [select-input
              {:class "mb-4"
-              :label "Role"
-              :name "role"
-              :value (:role @values)
+              :label "Trashed"
+              :name "trashed"
+              :value (:trashed @values)
               :on-change on-change}
              [:option {:value ""}]
-             [:option {:value "user"} "User"]
-             [:option {:value "owner"} "Owner"]])
-          [select-input
-           {:class "mb-4"
-            :label "Trashed"
-            :name "trashed"
-            :value (:trashed @values)
-            :on-change on-change}
-           [:option {:value ""}]
-           [:option {:value "with"} "With Trashed"]
-           [:option {:value "only"} "Only Trashed"]]]]
-        [:button {:class "px-4 border-r rounded-l md:px-6 hover:bg-gray-100 focus:outline-none focus:border-white focus:ring-2 focus:ring-indigo-400 focus:z-10"
-                  :on-click #(reset! opened? true)}
-         [:div {:class "flex items-baseline"}
-          [:span {:class "hidden text-gray-700 md:inline"} "Filter"]
-          [:svg {:class "w-2 h-2 text-gray-700 fill-current md:ml-2"
-                 :xmlns "http://www.w3.org/2000/svg"
-                 :view-box "0 0 961.243 599.998"}
-           [:path {:d "M239.998 239.999L0 0h961.243L721.246 240c-131.999 132-240.28 240-240.624 239.999-.345-.001-108.625-108.001-240.624-240z"}]]]]
-        [:input {:class "relative w-full px-6 py-3 rounded-r focus:outline-none focus:ring-2 focus:ring-indigo-400"
-                 :auto-complete "off"
-                 :type "text"
-                 :name "search"
-                 :value (:search @values)
-                 :on-change on-change
-                 :placeholder "Search..."}]]
-       [:button {:class "ml-3 text-sm text-gray-600 hover:text-gray-700 focus:text-indigo-700 focus:outline-none"
-                 :type "button"
-                 :on-click reset}
-        "Reset"]])))
+             [:option {:value "with"} "With Trashed"]
+             [:option {:value "only"} "Only Trashed"]]]]
+          [:button {:class "px-4 border-r rounded-l md:px-6 hover:bg-gray-100 focus:outline-none focus:border-white focus:ring-2 focus:ring-indigo-400 focus:z-10"
+                    :on-click #(reset! opened? true)}
+           [:div {:class "flex items-baseline"}
+            [:span {:class "hidden text-gray-700 md:inline"} "Filter"]
+            [:svg {:class "w-2 h-2 text-gray-700 fill-current md:ml-2"
+                   :xmlns "http://www.w3.org/2000/svg"
+                   :view-box "0 0 961.243 599.998"}
+             [:path {:d "M239.998 239.999L0 0h961.243L721.246 240c-131.999 132-240.28 240-240.624 239.999-.345-.001-108.625-108.001-240.624-240z"}]]]]
+          [:input {:class "relative w-full px-6 py-3 rounded-r focus:outline-none focus:ring-2 focus:ring-indigo-400"
+                   :auto-complete "off"
+                   :type "text"
+                   :name "search"
+                   :value (:search @values)
+                   :on-change on-change
+                   :placeholder "Search..."}]]
+         [:button {:class "ml-3 text-sm text-gray-600 hover:text-gray-700 focus:text-indigo-700 focus:outline-none"
+                   :type "button"
+                   :on-click reset}
+          "Reset"]])})))
