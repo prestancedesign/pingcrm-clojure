8147073f8b24f1cede8b3dfa544f7cd6323fb53b
Michael SALIHI <admin@prestance-informatique.fr>
2021-07-01 16:00:30 +0200

    Refactor SSR function

--- src/cljs/pingcrm/ssr.cljs
+++ src/cljs/pingcrm/ssr.cljs
@@ -11,17 +11,18 @@
   (let [port (or (.. js/process -env -PORT) 8000)]
     (-> (express)
         (.use (.json express))
-        (.post "/render" (fn [req res next]
-                           (try
-                             (-> (createInertiaApp
-                                  (clj->js {:resolve (fn [name]
-                                                       (let [^js comp (r/reactify-component (get pages name))]
-                                                         (set! (.-layout comp) (fn [page] (r/as-element [layout page])))
-                                                         comp))
-                                            :page (.-body req)
-                                            :render ssr/render-to-string
-                                            :setup (j/fn [^:js {:keys [App props]}]
-                                                     (r/as-element [:f> App props]))}))
-                                 (.then (fn [page] (.json res page))))
-                             (catch js/Error error (next error)))))
-        (.listen port))))
+        (.post "/render"
+               (fn [req res next]
+                 (try
+                   (-> (createInertiaApp
+                        #js {:resolve (fn [name]
+                                        (let [^js comp (r/reactify-component (get pages name))]
+                                          (set! (.-layout comp) (fn [page] (r/as-element [layout page])))
+                                          comp))
+                             :page (.-body req)
+                             :render ssr/render-to-string
+                             :setup (j/fn [^:js {:keys [App props]}]
+                                      (r/as-element [:f> App props]))})
+                       (.then (fn [page] (.json res page))))
+                   (catch js/Error error (next error)))))
+        (.listen port #(js/console.log "Server started.")))))
