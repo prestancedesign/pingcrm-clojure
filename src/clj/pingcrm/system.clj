cb7b81a3c20b463c68827e37fe89adde84e2d9e8
Michael SALIHI <admin@prestance-informatique.fr>
2021-07-01 08:20:04 +0200

    Replace Jetty with Http-kit + add JSON dependencie

--- src/clj/pingcrm/system.clj
+++ src/clj/pingcrm/system.clj
@@ -3,19 +3,19 @@
             [next.jdbc :as jdbc]
             [next.jdbc.result-set :as rs]
             [pingcrm.router :as router]
-            [ring.adapter.jetty :as server]))
+            [org.httpkit.server :as server]))
 
 (defn app [db]
   (router/routes db))
 
 (def config
-  {:server/jetty {:handler (ig/ref :pingcrm/app) :port 3000}
+  {:server/http {:handler (ig/ref :pingcrm/app) :port 3000}
    :pingcrm/app {:db (ig/ref :database.sql/connection)}
    :database.sql/connection {:dbtype "sqlite" :dbname "resources/database/database.sqlite"}})
 
-(defmethod ig/init-key :server/jetty [_ {:keys [handler port]}]
+(defmethod ig/init-key :server/http [_ {:keys [handler port]}]
   (println "\nServer running on port" port)
-  (server/run-jetty handler {:port port :join? false}))
+  (server/run-server handler {:port port :join? false}))
 
 (defmethod ig/init-key :pingcrm/app [_ {:keys [db]}]
   (app db))
@@ -24,8 +24,8 @@
   (let [ds (jdbc/get-datasource db-spec)]
     (jdbc/with-options ds {:builder-fn rs/as-unqualified-maps})))
 
-(defmethod ig/halt-key! :server/jetty [_ server]
-  (.stop server))
+(defmethod ig/halt-key! :server/http [_ server]
+  (server))
 
 (defn -main []
   (ig/init config))
