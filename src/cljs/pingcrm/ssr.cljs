e896d781bd86bde1f387ca332f3c45cf024eea08
Michael SALIHI <admin@prestance-informatique.fr>
2021-07-02 16:15:43 +0200

    Add missing condition to exclude layout from login page

--- src/cljs/pingcrm/ssr.cljs
+++ src/cljs/pingcrm/ssr.cljs
@@ -17,7 +17,8 @@
                    (-> (createInertiaApp
                         #js {:resolve (fn [name]
                                         (let [^js comp (r/reactify-component (get pages name))]
-                                          (set! (.-layout comp) (fn [page] (r/as-element [layout page])))
+                                          (when-not (= name "Auth/Login")
+                                            (set! (.-layout comp) (fn [page] (r/as-element [layout page]))))
                                           comp))
                              :page (.-body req)
                              :render ssr/render-to-string
