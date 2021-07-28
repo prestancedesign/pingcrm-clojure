672cd8fe6eaf2af790a7c19fda520a00ac8c8392
Michael SALIHI <admin@prestance-informatique.fr>
2021-07-29 00:32:03 +0200

    Use Inertia.reload with Shadow after-load hook to handle refresh

--- src/cljs/pingcrm/app.cljs
+++ src/cljs/pingcrm/app.cljs
@@ -1,20 +1,19 @@
 (ns pingcrm.app
-  (:require ["@inertiajs/inertia-react" :refer [createInertiaApp]]
+  (:require ["@inertiajs/inertia" :refer [Inertia]]
+            ["@inertiajs/inertia-react" :refer [createInertiaApp]]
             ["@inertiajs/progress" :refer [InertiaProgress]]
-            [reagent.core :as r]
-            [reagent.dom :as d]
+            [applied-science.js-interop :as j]
+            [pingcrm.pages.contacts :as contacts]
             [pingcrm.pages.dashboard :as dashboard]
-            [pingcrm.shared.layout :refer [layout]]
             [pingcrm.pages.login :refer [login]]
             [pingcrm.pages.organizations :as organizations]
-            [pingcrm.pages.contacts :as contacts]
             [pingcrm.pages.reports :as reports]
             [pingcrm.pages.users :as users]
-            [applied-science.js-interop :as j]))
+            [pingcrm.shared.layout :refer [layout]]
+            [reagent.core :as r]
+            [reagent.dom :as d]))
 
 (.init InertiaProgress)
-
-(def el (.getElementById js/document "app"))
 
 (def pages {"Dashboard/Index" dashboard/index
             "Auth/Login" login
@@ -29,15 +28,16 @@
             "Users/Create" users/create
             "Users/Edit" users/edit})
 
-(defn start []
+(defn init! []
   (createInertiaApp
    #js {:resolve (fn [name]
                    (let [^js comp (r/reactify-component (get pages name))]
-                     (set! (.-layout comp) (fn [page] (r/as-element [layout page])))
+                     (when-not (= name "Auth/Login")
+                       (set! (.-layout comp) (fn [page] (r/as-element [layout page]))))
                      comp))
         :title (fn [title] (str title " | Ping CRM"))
         :setup (j/fn [^:js {:keys [el App props]}]
                  (d/render (r/as-element [:f> App props]) el))}))
 
-(defn init! []
-  (start))
+(defn ^:dev/after-load reload []
+  (.reload Inertia))
